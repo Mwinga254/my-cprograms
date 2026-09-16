08be067018681bfa0c4dfbd0e7f76b8b169222cc
Jesse <jessemwinga34@gmail.com>
2026-09-16 20:10:44 +0300

    Add files via upload

---
+++ Academics unit offered.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("introduction to programming\n");
+	printf("Data structures\n");
+	printf("computer networks\n");
+	printf("Database systems\n");
+	printf("software Engineering");
+	
+	return 0;
+}
