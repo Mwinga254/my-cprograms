f596da82d3f37941c599698636431227ca856f87
Jesse <jessemwinga34@gmail.com>
2026-09-17 21:55:01 +0300

    Add files via upload

---
+++ week2 (calculating surface area) Task1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	float Height;//%f
+	int Radius;//%d
+	double volume;//%lf
+	double surfacearea;//%lf
+	const float pi = 3.14159;
+	
+	
+	
+	printf("Enter Height:\n");
+	scanf("%f", &Height);
+	
+	printf("Enter Radius: \n");
+	scanf("%d", &Radius);
+	
+	printf("calculate volume: \n");
+	volume = pi * Radius * Radius * Height;
+	
+	printf("calculate surface area: \n");
+	surfacearea = 2 * pi * Radius * Radius + 2 * pi * Radius * Height;
+	
+	printf("Height: %.2fm\n", Height);
+	printf("Radius: %dm\n", Radius);
+	printf("volume: %.2lfm^3\n", volume);
+	printf("Surfacearea: %.2lfm^2", surfacearea);
+	
+	
+	
+	return 0;
+}
